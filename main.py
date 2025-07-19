13e01c7afbe043abec2bef1262e542ccc878bfa3
MauroZn <zanottimauro93@gmail.com>
2025-07-19 15:36:07 +0200

    Changed get_image_colors() function completely to display the actual color too in the results

--- main.py
+++ main.py
@@ -38,32 +38,30 @@ def imageUploader():
         print("No file is chosen, please choose a file.")
 
 def get_image_colors():
-    all_colors = ""
-    #Transform the 3D Array into a 2D. Row:Pixel, Column: RGB
+    colors_list_box.delete('1.0', tk.END)
+
+    if img_array.size == 0:
+        colors_list_box.insert(tk.END, "No image loaded.\n")
+        return
+
+    # Transform the 3D Array into 2D (rows = pixels, cols = RGB)
     pixels = img_array.reshape(-1, img_array.shape[-1])
 
-    #Get Unique Colors and count how many times they are used in the image
-    unique_colors, counts = np.unique(pixels, axis=0, return_counts=True)
-
-    #Create a list.
     #Zip is used to combine multiple sequences element by element (In this case the tuple and counts)
     #Tuple is used to convert the color array, "Example:(255,255,255)", into a Python Tuple that is better for data management
     #So with this I create a list of RGB colors paired with counts to see how many of the same there are in the pic
+    unique_colors, counts = np.unique(pixels, axis=0, return_counts=True)
     color_counts = list(zip([tuple(color) for color in unique_colors], counts))
-
-    # Now I sort by count so after this I can get the 10 most used ones
     color_counts.sort(key=lambda x: x[1], reverse=True)
 
     for i, (color, count) in enumerate(color_counts[:10]):
-        #I cycle in the list to get the separated rgb values, so I can convert them to hex
-        r = color[0]
-        g = color[1]
-        b = color[2]
+        r, g, b = color
         hex_color = '#{:02x}{:02x}{:02x}'.format(r, g, b)
-        all_colors += str(i+1) + str(hex_color) + ":" + str(count) + "\n"
-        # print(f"{i+1}. Color {hex_color} appears {count} times")
 
-    colors_list_box.insert(tk.END, all_colors)
+        color_label = tk.Label(colors_list_box, bg=hex_color, width=2, height=1)
+        colors_list_box.insert(tk.END, f"{i+1}. {hex_color} ")
+        colors_list_box.window_create(tk.END, window=color_label)
+        colors_list_box.insert(tk.END, "\n")
 
 if __name__ == "__main__":
 
